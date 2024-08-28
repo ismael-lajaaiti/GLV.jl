4b28125fac537ddd7c6f1f10fde39ea759e98b39
Documenter.jl <documenter@juliadocs.github.io>
2024-08-28 14:58:47 +0000

    build based on 8033ad8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GLV.jl-documentation","page":"GLV.jl documentation","title":"GLV.jl documentation","text":"","category":"section"},{"location":"","page":"GLV.jl documentation","title":"GLV.jl documentation","text":"Welcome to the documentation for GLV.jl.","category":"page"},{"location":"","page":"GLV.jl documentation","title":"GLV.jl documentation","text":"Modules = [GLV]","category":"page"},{"location":"","page":"GLV.jl documentation","title":"GLV.jl documentation","text":"using GLV\nGLV.greet()","category":"page"}]
+}
